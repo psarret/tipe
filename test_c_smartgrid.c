eda85779b7b366f9d22f8b7dc9d85ba8000b3b4d
psarret <pierres2807@gmail.com>
2022-10-20 17:15:13 +0200

    20/10/2022_17:15

--- test_c_smartgrid.c
+++ test_c_smartgrid.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 typedef struct node
 {
@@ -29,17 +31,41 @@ void set_power_line(node * source, node * conso, int adj[5][5])
 {
   if(source->debit + conso->debit <= 0)
     {
-      printf("production insuffisante\n");
+      printf("production insuffisante pour consommateur %d\n",conso->id);
     }
    
   if(source->debit + conso->debit > 0)
     {
       source->debit += conso->debit;
-      adj[source->id][conso->id] = 1;
-      adj[conso->id][source->id] = 1;
+      adj[source->id-1][conso->id-1] = 1;
+      adj[conso->id-1][source->id-1] = 1;
     }
 }
 
+
+void print_mat_adj(int ajd[5][5])
+{
+  for(int i=0; i<5; i++)
+    {
+      for(int j=0; j<5; j++)
+	{
+	  printf("%d",ajd[i][j]);
+	}
+      printf("\n");
+    }
+}
+
+void test ()
+{
+  for(int i=0; i<100; i++)
+    {
+      system("clear");
+      printf("test %d",i);
+      fflush(stdout);
+      sleep(1);
+    }
+  
+}
 
 int main()
 {
@@ -55,12 +81,27 @@ int main()
   node source = {1,'n',10.0};
   node consomateur = {2,'c',-1.0};
   */
-  node * source = create_node(1,'n',10.0);
-  node * consomateur = create_node(2,'c',-1.0);
+  node * source1 = create_node(1,'n',10.0);
+  node * consomateur2 = create_node(2,'c',-1.0);
+  node * consomateur3 = create_node(3,'c',-3.0);
+  node * consomateur4 = create_node(4,'c',-9.0);
+  node * consomateur5 = create_node(5,'c',-1.0);
+  
+  set_power_line(source1,consomateur2,mat_adj);
+  set_power_line(source1,consomateur3,mat_adj);
+  set_power_line(source1,consomateur4,mat_adj);
+  set_power_line(source1,consomateur5,mat_adj);
+  
+  printf("débit source : %f\n",source1->debit);
+  print_mat_adj(mat_adj);
+  test();
 
-  set_power_line(source,consomateur,mat_adj);
+/*
 
-  printf("débit source : %f\n",source->debit);
+system("clear") 
+getchar()
+
+*/
   
   return 0;
 }
